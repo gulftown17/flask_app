c591cacd7ff509a1bf3b2b805e58b4ce613fe576
gulftown17 <126068381+gulftown17@users.noreply.github.com>
2023-06-08 13:59:21 +0200

    Update meme_app.py
    updated url

--- meme_app.py
+++ meme_app.py
@@ -10,7 +10,7 @@ def get_meme():
     #Uncomment these two lines and comment out the other url line if you want to use a specific meme subreddt
     # sr = "/wholesomememes"
     # url = "<https://meme-api.herokuapp.com/gimme>" + sr
-    url = "https://meme-api.herokuapp.com/gimme"
+    url = "https://meme-api.com/gimme"
     response = json.loads(requests.request("GET", url).text)
     meme_large = response["preview"][-2]
     subreddit = response["subreddit"]
